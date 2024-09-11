b985137eb5b7f1529859e7d4d78da536162bcccd
Wachiice <wachiraya.ph@kkumail.com>
2024-09-11 20:39:06 +0700

    Refactor code PhoneNumber JavaScript and Stimulus

--- app/javascript/controllers/phone_controller.js
+++ app/javascript/controllers/phone_controller.js
@@ -1,12 +1,23 @@
-document.addEventListener("turbo:load", () => { 
-    const phoneInput = document.getElementById("phone");
+import { Controller } from "@hotwired/stimulus";
 
-    phoneInput?.addEventListener("input", () => {
-        let value = phoneInput.value.replace(/\D/g, "");
-        phoneInput.value = value.length > 6 
-            ? `${value.slice(0, 3)}-${value.slice(3, 6)}-${value.slice(6, 10)}`
-            : value.length > 3 
-            ? `${value.slice(0, 3)}-${value.slice(3)}`
-            : value;
-    });
-});
+export default class extends Controller {
+  static targets = ["input"];
+
+  connect() {
+    this.inputTarget.addEventListener("input", this.format.bind(this));
+    console.log('test');
+  }
+
+  format(event) {
+    let value = event.target.value.replace(/\D/g, ''); // Remove non-digit characters
+    let x = value.match(/(\d{0,3})(\d{0,3})(\d{0,4})/); // Capture groups for formatting
+    
+    if (x) {
+      // Construct formatted phone number in the xxx-xxx-xxxx format
+      let formattedValue = x[1] 
+        + (x[2] ? '-' + x[2] : '') 
+        + (x[3] ? '-' + x[3] : '');
+      event.target.value = formattedValue; // Apply formatted value
+    }
+  }
+}
