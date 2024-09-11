943d4ef251de7d3454b43b393d4428b1541ce0c4
Wachiice <wachiraya.ph@kkumail.com>
2024-09-11 19:26:07 +0700

    refactor code phone_controller.js

--- app/javascript/controllers/phone_controller.js
+++ app/javascript/controllers/phone_controller.js
@@ -1,15 +1,12 @@
-document.addEventListener("turbo:load", function() { 
+document.addEventListener("turbo:load", () => { 
     const phoneInput = document.getElementById("phone");
 
-    if (phoneInput) {
-      phoneInput.addEventListener("input", function(e) {
-        let value = phoneInput.value.replace(/\D/g, ""); 
-        if (value.length > 3 && value.length <= 6) {
-          value = `${value.slice(0, 3)}-${value.slice(3)}`;
-        } else if (value.length > 6) {
-          value = `${value.slice(0, 3)}-${value.slice(3, 6)}-${value.slice(6, 10)}`;
-        }
-        phoneInput.value = value;
-      });
-    }
+    phoneInput?.addEventListener("input", () => {
+        let value = phoneInput.value.replace(/\D/g, "");
+        phoneInput.value = value.length > 6 
+            ? `${value.slice(0, 3)}-${value.slice(3, 6)}-${value.slice(6, 10)}`
+            : value.length > 3 
+            ? `${value.slice(0, 3)}-${value.slice(3)}`
+            : value;
+    });
 });
